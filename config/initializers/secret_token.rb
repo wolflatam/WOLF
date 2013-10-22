74f54ea749a13433fcec377ebb5fb2f43b437b5a
Sebastian Parisi <seba@wolflatam.com>
2013-10-22 19:42:15 -0400

    first project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WOLF::Application.config.secret_token = '6d646367678ffacdefd962502250fb23df1ffe2c8ae55a24baa9de808e32943af78f35b65a59737618f7bc66509a916576bf75cebb439b9e96dc38bc8d63b342'
